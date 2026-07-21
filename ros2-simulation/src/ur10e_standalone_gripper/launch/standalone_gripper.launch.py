eb90d55ddf1653ee5abf05f697d4cb1915657f4a
Sidhant Manoj Barai <sidhantmb@gmail.com>
2026-07-21 10:36:46 +0100

    standalone_gripper.launch.py

---
+++ ros2-simulation/src/ur10e_standalone_gripper/launch/standalone_gripper.launch.py
@@ -0,0 +1,35 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg_dir = get_package_share_directory('ur10e_standalone_gripper')
+    xacro_file = os.path.join(pkg_dir, 'urdf', 'ur10e_robotiq_standalone.urdf.xacro')
+
+    # Process Xacro to URDF parameter
+    robot_description_raw = Command(['xacro ', xacro_file])
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_description_raw}]
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            output='screen'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen'
+        )
+    ])
